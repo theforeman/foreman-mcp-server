fca7f697de960695b16f7596bac27dba26c1724c
Adam Ruzicka <aruzicka@redhat.com>
2026-02-09 16:19:55 +0100

    Unify auth middlewares

--- src/foreman_mcp_server/middleware/auth.py
+++ src/foreman_mcp_server/middleware/auth.py
@@ -3,13 +3,26 @@ from fastmcp.server.middleware import Middleware, MiddlewareContext
 
 
 class AuthMiddleware(Middleware):
-    def __init__(self, foreman_url: str, verify_ssl):
+    def __init__(
+        self, foreman_url: str, verify_ssl: bool, foreman_api: apypie.ForemanApi | None
+    ):
         # TODO: user_map can grow indefinitely, consider using a more efficient structure or cleanup mechanism
         self.user_map = {}
         self.foreman_url = foreman_url
         self.verify_ssl = verify_ssl
+        self.foreman_api = foreman_api
 
     async def on_request(self, context: MiddlewareContext, call_next):
+        if context.fastmcp_context:
+            foreman_api = self.foreman_api or self.get_foreman_api_from_request(context)
+            if foreman_api:
+                context.fastmcp_context.foreman_api = foreman_api
+
+        return await call_next(context)
+
+    def get_foreman_api_from_request(
+        self, context: MiddlewareContext
+    ) -> apypie.ForemanApi | None:
         # Add the foreman_api to the context for use in tools
         if context.fastmcp_context and context.fastmcp_context.request_context:
             foreman_username = (
@@ -37,6 +50,5 @@ class AuthMiddleware(Middleware):
                 )
                 self.user_map[foreman_username] = {}
                 self.user_map[foreman_username][session_id] = foreman_api
-            context.fastmcp_context.user_map = self.user_map
 
-        return await call_next(context)
+            return self.user_map[foreman_username][session_id]
