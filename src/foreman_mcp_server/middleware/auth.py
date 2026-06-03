b4a07ece328dbde9746b286d949c43cd7a12d21f
Adam Ruzicka <aruzicka@redhat.com>
2026-06-03 14:30:13 +0200

    Do not cache foreman api instance

--- src/foreman_mcp_server/middleware/auth.py
+++ src/foreman_mcp_server/middleware/auth.py
@@ -6,8 +6,6 @@ class AuthMiddleware(Middleware):
     def __init__(
         self, foreman_url: str, verify_ssl: bool, foreman_api: apypie.ForemanApi | None
     ):
-        # TODO: user_map can grow indefinitely, consider using a more efficient structure or cleanup mechanism
-        self.user_map = {}
         self.foreman_url = foreman_url
         self.verify_ssl = verify_ssl
         self.foreman_api = foreman_api
@@ -23,7 +21,6 @@ class AuthMiddleware(Middleware):
     def get_foreman_api_from_request(
         self, context: MiddlewareContext
     ) -> apypie.ForemanApi | None:
-        # Add the foreman_api to the context for use in tools
         if context.fastmcp_context and context.fastmcp_context.request_context:
             foreman_username = (
                 context.fastmcp_context.request_context.request.headers.get(
@@ -37,18 +34,9 @@ class AuthMiddleware(Middleware):
                 raise RuntimeError(
                     "Foreman username and token must be provided in headers."
                 )
-            session_id = context.fastmcp_context.request_context.request.headers.get(
-                "mcp-session-id"
+            return apypie.ForemanApi(
+                uri=self.foreman_url,
+                username=foreman_username,
+                password=foreman_token,
+                verify_ssl=self.verify_ssl,
             )
-            if session_id not in self.user_map.get(foreman_username, {}):
-                # Reset foreman_api for the user keeping the same credentials for the same session
-                foreman_api = apypie.ForemanApi(
-                    uri=self.foreman_url,
-                    username=foreman_username,
-                    password=foreman_token,
-                    verify_ssl=self.verify_ssl,
-                )
-                self.user_map[foreman_username] = {}
-                self.user_map[foreman_username][session_id] = foreman_api
-
-            return self.user_map[foreman_username][session_id]
